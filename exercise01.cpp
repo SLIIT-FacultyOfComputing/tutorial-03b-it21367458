0be5af49ab4a4d675f6aec110b8affb7dc42b9e3
IT21367458 <99566405+it21367458@users.noreply.github.com>
2022-02-23 23:50:53 +0000

    completed3.1

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
+  //Define variables
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
+  int r;
+  //printing the out put
+  cout<<setw(5)<<"No"<<setw(15)<<"Nmae"<<setw(10)<<"Marks"<<endl;
+//creat for loop
  for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
+     cout << setw(5) << r+1 
+          << setw(15)<<  names[r]
+          << setw(10)<<setiosflags(ios::fixed)<<setprecision(2) <<     marks[r] << endl;
  }
 }
